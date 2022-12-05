8ce773db8edd62999ccfb3cd964e98a5cce87603
Paul Wild <paul.wild@coop.co.uk>
2022-12-05 16:02:03 +0000

    Day 05

--- AdventOfCode/Days/Day05.cs
+++ AdventOfCode/Days/Day05.cs
@@ -7,69 +7,50 @@ public partial class Day05 : ISolution
 {
     public string PartOne(IEnumerable<string> input)
     {
-        List<List<char>> rawStacks = new();
-        List<(int Count, int From, int To)> instructions = new();
-        
-        //parsing the data. Gross
-        foreach (var row in input)
-        {
-            if (row.Contains('['))
-            {
-                var counter = 0; 
-                foreach (var chars in row.Chunk(4))
-                {
-  
-                    if (rawStacks.Count < counter+1)
-                    {
-                        rawStacks.Add(new List<char>());
-                    }
+        var (stacks, instructions) = ParseInput(input);
 
-                    if (chars[1] != ' ')
-                    {
-                        rawStacks[counter].Add(chars[1]);
-                    }
-                    counter++;
-                }
-            }
-            if (row.Contains("move"))
-            {
-                var numbers = NumbersRegex().Matches(row);
-                instructions.Add((int.Parse(numbers[0].Value), int.Parse(numbers[1].Value), int.Parse(numbers[2].Value)));
-            }
-        }
-
-        var stacks = new List<Stack<char>>();
-        foreach (var rawStack in rawStacks)
-        {
-            rawStack.Reverse();
-            stacks.Add(new Stack<char>(rawStack));
-        }
-        
-        //moving things around
         foreach (var instruction in instructions)
         {
             var tmpQueue = new Queue<char>();
  
             for (var i = 0; i < instruction.Count; i++)
             {
-                tmpQueue.Enqueue(stacks[instruction.From-1].Pop());
+                tmpQueue.Enqueue(stacks[instruction.From - 1].Pop());
             }
 
             while (tmpQueue.Count > 0)
             {
-                stacks[instruction.To -1].Push(tmpQueue.Dequeue());
+                stacks[instruction.To - 1].Push(tmpQueue.Dequeue());
             }
         }
-
-        var toReturn = "";
-        foreach (var stack in stacks)
-        {
-            toReturn = toReturn + stack.Pop();
-        }
-        return toReturn;
+        
+        return string.Join("", stacks.Select(stack => stack.Pop()));
     }
 
     public string PartTwo(IEnumerable<string> input)
+    {
+        var (stacks, instructions) = ParseInput(input);
+
+        foreach (var instruction in instructions)
+        {
+            var tmpQueue = new Stack<char>();
+ 
+            for (var i = 0; i < instruction.Count; i++)
+            {
+                tmpQueue.Push(stacks[instruction.From - 1].Pop());
+            }
+
+            while (tmpQueue.Count > 0)
+            {
+                stacks[instruction.To - 1].Push(tmpQueue.Pop());
+            }
+        }
+
+        return string.Join("", stacks.Select(stack => stack.Pop()));
+    }
+
+    private (List<Stack<char>> stacks, List<(int Count, int From, int To)> instructions) ParseInput(
+        IEnumerable<string> input)
     {
         List<List<char>> rawStacks = new();
         List<(int Count, int From, int To)> instructions = new();
@@ -109,30 +90,10 @@ public partial class Day05 : ISolution
             rawStack.Reverse();
             stacks.Add(new Stack<char>(rawStack));
         }
-        
-        //moving things around
-        foreach (var instruction in instructions)
-        {
-            var tmpQueue = new Stack<char>();
- 
-            for (var i = 0; i < instruction.Count; i++)
-            {
-                tmpQueue.Push(stacks[instruction.From-1].Pop());
-            }
 
-            while (tmpQueue.Count > 0)
-            {
-                stacks[instruction.To -1].Push(tmpQueue.Pop());
-            }
-        }
-
-        var toReturn = "";
-        foreach (var stack in stacks)
-        {
-            toReturn = toReturn + stack.Pop();
-        }
-        return toReturn;
+        return (stacks, instructions);
     }
+    
     public int Day => 05;
     
     [GeneratedRegex("\\d+")]
